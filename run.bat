c85fa0f34b9dee36ad591a0fb664c0a838ed1c28
ycllz <490779880@qq.com>
2021-01-04 18:36:23 +0800

    Create run.bat
    init

---
+++ run.bat
@@ -0,0 +1,7 @@
+echo "# chromium-and-webkit" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ycllz/chromium-and-webkit.git
+git push -u origin main
